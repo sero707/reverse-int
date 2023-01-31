4517bdbae85b322446a2c4293d5ba5f9959898c9
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2023-01-31 19:16:15 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+		
+    let strN = String(n).split("");
+    if(strN[0] === '-'){
+        strN = strN.slice(1, strN.length);
+    }
+    strN = strN.reverse();
+    if(strN[0] === '0'){
+        strN = strN.slice(1, strN.length);
+    }
+
+    return Number(strN.join(''));
 }
